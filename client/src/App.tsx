68166913be3c1cf31391d83477dd744fd60a3bb4
SRoy555 <sr8295679@gmail.com>
2025-08-11 18:50:29 +0530

    Add CreateTrip and AddActivities components with routing and API integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
+import AddActivities from './pages/AddActivities';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Landing from './pages/Landing';
+import CreateTrip from './pages/CreateTrip';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Auth />} />
         <Route path="/signup" element={<Auth isSignUp={true} />} />
+                <Route path="/trips/create" element={<CreateTrip />} />
+                <Route path="/trips/:tripId/activities" element={<AddActivities />} />
       </Routes>
     </Router>
   );
